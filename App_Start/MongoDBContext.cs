7e3c1723b1b605e4ab3b367bcf978e2aad524ec1
Guilherme Xavier <61256795+guiixav@users.noreply.github.com>
2020-05-26 13:07:10 -0300

    Add files via upload

---
+++ App_Start/MongoDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using System.Configuration;
+
+namespace RoxierMongoDB.App_Start
+{
+    public class MongoDBContext
+    {
+        MongoClient client;
+        public IMongoDatabase database;
+
+
+        public MongoDBContext ()
+        {
+            var mongoClient = new MongoClient(ConfigurationManager.AppSettings["conexaoMongoDB"]);
+            database = mongoClient.GetDatabase(ConfigurationManager.AppSettings["MongoDB"]);
+        }
+    }
+}
